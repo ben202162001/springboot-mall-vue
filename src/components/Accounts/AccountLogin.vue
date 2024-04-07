3b68267bf36ba7e1bbcf7d80dfa3341d652c3032
ben202162001 <153347699+ben202162001@users.noreply.github.com>
2024-04-07 12:25:45 +0800

    把user的回傳改用ErrorMassage做回傳
    把user的回傳改用ErrorMassage做回傳

--- src/components/Accounts/AccountLogin.vue
+++ src/components/Accounts/AccountLogin.vue
@@ -1,49 +1,50 @@
 <template>
-    <div>
-      <input v-model="phone_Number" type="text" placeholder="Phone Number">
-      <input v-model="password" type="password" placeholder="Password">
-      <button @click="login">Login</button>
-      <p v-if="isLoading">Loading...</p>
-      <p v-else-if="error">Error: {{ error }}</p>
-      <div v-else>
-        <p>User ID: {{ user.userId }}</p>
-        <p>User Name: {{ user.userName }}</p>
-        <p>Password: {{ user.password }}</p>
-        <p>Phone Number: {{ user.phoneNumber }}</p>
-        <p>Create Time: {{ user.createTime }}</p>
-        <p>Edit Time: {{ user.editTime }}</p>
-      </div>
+  <div>
+    <input v-model="phone_Number" type="text" placeholder="Phone Number">
+    <input v-model="password" type="password" placeholder="Password">
+    <button @click="login">Login</button>
+    <p v-if="isLoading">Loading...</p>
+    <p v-else-if="error">{{ error }}</p>
+    <div v-else>
+      <p>massageStatus: {{ massageStatus }}</p>
+      <p>Error Time: {{ errorTime }}</p>
+      <p>{{ message }}</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        phone_Number: '',
-        password: '',
-        user: {},
-        isLoading: false,
-        error: null
-      };
-    },
-    methods: {
-      async login() {
-        try {
-          this.isLoading = true;
-          const url = `http://localhost:8080/login?phone_Number=${this.phone_Number}&password=${this.password}`;
-          const response = await fetch(url);
-          if (!response.ok) {
-            throw new Error('Failed to fetch user data');
-          }
-          this.user = await response.json();
-        } catch (error) {
-          this.error = error.message;
-        } finally {
-          this.isLoading = false;
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      phone_Number: '',
+      password: '',
+      MassageStatus: '',
+      errorTime: '',
+      message: '',
+      isLoading: false,
+      error: null
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        this.isLoading = true;
+        const url = `http://localhost:8080/login?phone_Number=${this.phone_Number}&password=${this.password}`;
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
         }
+        const responseData = await response.json();
+        this.massageStatus = responseData.massageStatus;
+        this.errorTime = responseData.errorTime;
+        this.message = responseData.massage;
+      } catch (error) {
+        this.error = error.message;
+      } finally {
+        this.isLoading = false;
       }
     }
-  };
-  </script>
-  
+  }
+};
+</script>
